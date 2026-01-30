70eebcecfbee1526fa244b150d0d299f2ab7b6c8
Mosheg <babagilboa@gmail.com>
2026-01-30 15:38:21 +0200

    Random mask

--- Assets/Scripts/Character/MaskChanger.cs
+++ Assets/Scripts/Character/MaskChanger.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class MaskChanger : MonoBehaviour
 {
+	public GameObject[] Masks_Array;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    	for(int i=0 ; i<=7 ; i++)
+    	{
+    		Masks_Array[i].SetActive(false);
+    	}
+        int randomInt = Random.Range(0,7);
+        {
+        	Masks_Array[randomInt].SetActive(true);
+        }
+
     }
 
     // Update is called once per frame
